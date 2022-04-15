867241d62163023c24d49c6d32ec63d8debeeb4f
Bhumit <bhumit.belani@hiddenbrains.in>
2022-04-15 14:27:30 +0530

    icons updated

--- app/src/main/java/com/app/permissionhub/MainActivity.kt
+++ app/src/main/java/com/app/permissionhub/MainActivity.kt
@@ -22,7 +22,9 @@ class MainActivity : AppCompatActivity() {
             PermissionConfig.get().permission(PermissionName.CAMERA.setRequired()) // add setRequired if permission is must required for your App
                 .title(getString(R.string.permission_camera_title)) // Add permission title to be showed up on permission screen
                 .description(getString(R.string.permission_camera_desc)) // Add permission desc to be showed up on permission screen
-                .afterDeniedDescription(getString(R.string.permission_camera_after_deny_desc)), // Add permission desc to be showed up after permission is denied permanently from user
+                .afterDeniedDescription(getString(R.string.permission_camera_after_deny_desc)) // Add permission desc to be showed up after permission is denied permanently from user
+                .iconResource(R.drawable.ic_outline_camera_new) // Optional: Add permission icon resource to add your own icon on permission screen, default material icon is already there in library
+                .iconColorResource(R.color.purple_700), // Optional: Add theme color to be appliend on list screen, by default library will take default accent color of root project
             PermissionConfig.get().permission(PermissionName.ACCESS_FINE_LOCATION)// add setRequired if permission is must required for your App
                 .title(getString(R.string.permission_location_title))// Add permission title to be showed up on permission screen
                 .description(getString(R.string.permission_location_desc))// Add permission desc to be showed up on permission screen
